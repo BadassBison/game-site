8bb8a09bf774829c9e3e510af37f828da52c4256
Shawn Scott <shawn.scott.xd@gmail.com>
2020-09-20 15:29:59 -0500

    Add tests for dots game

--- src/app/games/dots/dots.component.spec.ts
+++ src/app/games/dots/dots.component.spec.ts
@@ -1,4 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from '../../shared/shared.module';
 
 import { DotsComponent } from './dots.component';
 
@@ -8,7 +11,11 @@ describe('DotsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DotsComponent ]
+      declarations: [ DotsComponent ],
+      imports: [
+        SharedModule,
+        BrowserAnimationsModule
+       ]
     })
     .compileComponents();
   }));
@@ -22,4 +29,48 @@ describe('DotsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('initial view menu', () => {
+
+    it('should show the start menu', () => {
+      const menu = fixture.debugElement.query(By.css('.start-screen'));
+      expect(menu).toBeTruthy();
+    });
+
+  });
+
+  describe('#startGame', () => {
+
+    beforeEach(() => {
+      component.startGame();
+    });
+
+    it('should set property \'gameStarted\' to true', () => {
+      expect(component.gameStarted).toBeTruthy();
+    });
+
+    it('should set property \'gameInitialized\' to false', () => {
+      expect(component.gameInitialized).toBeFalsy();
+    });
+
+    it('should have an undefined board', () => {
+      expect(component.board).toBeUndefined();
+    });
+
+    describe('after change detection', () => {
+
+      beforeEach(() => {
+        fixture.detectChanges();
+      });
+
+      it('should set property \'gameInitialized\' to true after view is checked', () => {
+        expect(component.gameInitialized).toBeTruthy();
+      });
+
+      it('should build a new board', () => {
+        expect(component.board).toBeDefined();
+      });
+    });
+  });
+
 });
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,7 +7,12 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRippleModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @NgModule({
   imports: [
@@ -19,7 +24,11 @@ import { MatSelectModule } from '@angular/material/select';
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
-    MatSelectModule
+    MatListModule,
+    MatMenuModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatToolbarModule
   ],
   exports: [
     CommonModule,
@@ -30,7 +39,11 @@ import { MatSelectModule } from '@angular/material/select';
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
-    MatSelectModule
+    MatListModule,
+    MatMenuModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatToolbarModule
   ]
 })
 export class SharedModule { }
